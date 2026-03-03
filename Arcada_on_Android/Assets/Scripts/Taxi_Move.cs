dadd1972fca3eb078106ae16ee1e9e4fe0bc31a8
Nikita <80509505+ElCucuy-08@users.noreply.github.com>
2026-03-03 22:06:08 +0300

    Animation_Car

---
+++ Arcada_on_Android/Assets/Scripts/Taxi_Move.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taxi_Move : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    float hor;
+    Vector3 size, inversize;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        size = inversize = transform.localScale;
+        inversize.x *= -1;
+    }
+
+    private void Update()
+    {
+        hor = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        rb.transform.Translate(Vector2.right * Time.fixedDeltaTime * 5 * hor);
+        DoAnim();
+    }
+    void DoAnim()
+    {
+        if(hor>0)
+        {
+            transform.localScale=size;
+        }
+        else if(hor<0)
+        {
+            transform.localScale=inversize;
+        }
+        anim.SetFloat("Move", Mathf.Abs(hor));
+    }
+}
